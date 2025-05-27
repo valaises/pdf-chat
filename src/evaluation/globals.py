58507012753a8dcfb90e15be37465054780ae06c
V4LER11 <work@valerii.cc>
2025-05-27 16:01:27 +0100

    oops, wrong env

--- src/evaluation/globals.py
+++ src/evaluation/globals.py
@@ -7,5 +7,5 @@ EVAL_USER_ID: int = 1337_8413211
 DB_EVAL_DIR = DB_DIR / "eval"
 DB_EVAL_DIR.mkdir(parents=True, exist_ok=True)
 
-EVAL_CHAT_ENDPOINT = os.environ.get("EVAL_CHAT_ENDPOINT_API_KEY")
+EVAL_CHAT_ENDPOINT = os.environ.get("EVAL_CHAT_ENDPOINT")
 EVAL_CHAT_ENDPOINT_API_KEY = os.environ.get("EVAL_CHAT_ENDPOINT_API_KEY")
